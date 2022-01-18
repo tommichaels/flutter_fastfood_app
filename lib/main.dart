9dcb16c513126f9ec93feb61f6fedd809d65879d
thetcruise <78617644+tommichaels@users.noreply.github.com>
2022-01-18 21:53:35 +0100

    Fixdetailsscreen

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
        scaffoldBackgroundColor: Colors.white,
        textTheme: const TextTheme(bodyText1: TextStyle(color: ksecondaryColor))
       ),
-      home: HomeScreen(),
+      home: DetailsScreen(),
     );
   }
 }
